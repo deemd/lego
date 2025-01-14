abd39042f11c8aa5fca8456ef1715a16417bcddb
Héloïse Robin <heloise.robin.pro@gmail.com>
2025-01-14 14:12:18 +0100

    feat(TODO 6): filter spec discount range

--- client/v1/index.js
+++ client/v1/index.js
@@ -77,6 +77,8 @@ console.log('Deals sorted by date:', dealsSortedByDate);
 // 🎯 TODO 6: Filter a specific percentage discount range
 // 1. Filter the list of deals between 50% and 75%
 // 2. Log the list
+const filteredDeals = deals.filter(deal => deal.discount >= 50 && deal.discount <= 75);
+console.log('Deals with discounts between 50% and 75%:', filteredDeals);
 
 // 🎯 TODO 7: Average percentage discount
 // 1. Determine the average percentage discount of the deals
